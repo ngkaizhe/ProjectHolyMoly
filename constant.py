330fc8bdc319a1970df9a8371b8ac7885942d9ad
Ng Kai Zhe <kaizhe1991@hotmail.com>
2019-06-03 03:53:42 +0800

    add constant.py

---
+++ constant.py
@@ -0,0 +1,23 @@
+# all constant  value should save here
+
+# [method_name, method_shortcut]
+method_shortcut = [
+    ['Fast Fourier Transform', 'Ctrl+F'],
+    ['Inverse Fast Fourier Transform', 'Ctrl+Shift+F'],
+    ['Lowpass Filter', 'Ctrl+L'],
+    ['Highpass Filter', 'Ctrl+H'],
+    ['Discrete Fourier Transform', 'Ctrl+D'],
+    ['Inverse Discrete Fourier Transform', 'Ctrl+Shift+D'],
+]
+
+window_x_pos = 350
+window_y_pos = 100
+window_width = 1200
+window_height = 900
+
+source_image_background_color = 'green'
+result_image_background_color = 'gray'
+
+image_need_rescale = True
+
+image_file_type = '(*.bmp *.jpg *.jpeg *.png)'
