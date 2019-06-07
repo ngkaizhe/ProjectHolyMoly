21d6c40746d9b1139787b176f237a93dae9fffca
Ng Kai Zhe <kaizhe1991@hotmail.com>
2019-06-07 20:53:38 +0800

    Finished Manager.py

--- Manager.py
+++ Manager.py
@@ -1,32 +1,42 @@
 from PyQt5.QtGui import QPixmap, QImage, QColor
 from constant import method_shortcut
 from method.Grey import Grey
-
+from method.DFT import DFT
+from method.IDFT import IDFT
+from method.FFT import FFT
+from method.IFFT import IFFT
+from method.HF import HF
+from method.LF import LF
 
 function_dict = {
-method_shortcut[0][0]: None,
+method_shortcut[0][0]: FFT,
+method_shortcut[1][0]: IFFT,
+method_shortcut[2][0]: LF,
+method_shortcut[3][0]: HF,
+method_shortcut[4][0]: DFT,
+method_shortcut[5][0]: IDFT,
 }
 
 
 def run_method(method_name: str, img: QImage) -> QPixmap:
-    colors_matrix = []
     output = QImage(img.width(), img.height(), img.format())
 
-    for r in range(img.width()):
-        temp = []
-        for c in range(img.height()):
-            value = img.pixel(r, c)
-            colors = QColor(value).getRgb()
-            temp.append(colors)
-        colors_matrix.append(temp)
-
     # run method here
-    result_matrix = Grey(colors_matrix)
+    result_matrix = function_dict[method_name](img)
 
-    for r in range(output.width()):
-        for c in range(output.height()):
-            color = result_matrix[r][c]
-            output.setPixelColor(r, c, color)
+    # means function are not done yet
+    if result_matrix is None:
+        return
+
+    # return the answer
+    for h in range(output.height()):
+        for w in range(output.width()):
+            if result_matrix[w][h] > 255:
+                result_matrix[w][h] = 255
+            elif result_matrix[w][h] < 0:
+                result_matrix[w][h] = 0
+            grey = result_matrix[w][h]
+            output.setPixelColor(w, h, QColor(grey, grey, grey, 255))
 
     result_pixmap = QPixmap().fromImage(output)
     return result_pixmap
